1722bdb54aba7e209d3c7ad6b73bae9565bb6df3
preyansh98 <preyansh98@gmail.com>
2020-09-21 17:03:59 -0400

    use CountVectorizer to create feature vector + lemmatize/stem options

--- a1.py
+++ a1.py
@@ -1,9 +1,12 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 import numpy as np
 import sklearn
+from sklearn.feature_extraction.text import CountVectorizer
 import string
+import re
 
 DATA_ENCODING = "ISO-8859-1"
 TRAIN_TEST_SPLIT = 0.75 #define ratio of dataset used for training vs testing
@@ -31,16 +34,39 @@ def read_data():
     return ([x_train, y_train], [x_test, y_test])
 
 def preprocess(x_train, y_train, N_train):
-    processed = []
     stopwords_set = set(stopwords.words('english'))
+
+    stemmer = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+
+    # define a preprocessor with stemmer
+    def stemmer_preprocessor(text):
+        text = text.lower()
+        text = re.sub("\\W", " ", text)
+
+        # stem words
+        words = word_tokenize(text)
+        filtered_words = [w for w in words if not w in stopwords_set and w not in string.punctuation]
+        stemmed_words = [stemmer.stem(word = word) for word in filtered_words]
+        return ' '.join(stemmed_words)
+
+    def lemmatizer_preprocessor(text):
+        text = text.lower()
+        text = re.sub("\\W", " ", text)
+
+        # lemmatize words
+        words = word_tokenize(text)
+        filtered_words = [w for w in words if not w in stopwords_set and w not in string.punctuation]
+        lemmatized_words = [lemmatizer.lemmatize(word = word) for word in filtered_words]
+        return ' '.join(lemmatized_words)
+
+    # remove stopwords and ignore words that appear in less than 2 samples
+    cv = CountVectorizer(min_df = 2, preprocessor=lemmatizer_preprocessor)
+    x_traincv = cv.fit_transform(x_train)
     
-    for sentence in x_train:
-        words_tokenized = word_tokenize(sentence)
- 
-        # remove stopwords and punctuation
-        filtered_sentence = [w for w in words_tokenized if not w in stopwords_set and w not in string.punctuation]
+    feature_vector = x_traincv.toarray()
+    print(feature_vector.shape)
     
-        # todo: create feature vectors
 
 def __init__():
     # first read training and test data
